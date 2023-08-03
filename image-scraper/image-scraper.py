250a6adf8ca78f785f07e3363e7caadeee56186c
emielvb <emiel.vd.b@hotmail.com>
2023-08-03 11:52:11 +0200

    Successfully download image from image url

--- image-scraper/image-scraper.py
+++ image-scraper/image-scraper.py
@@ -11,3 +11,20 @@ import time
 # PATH = "C:\\Program Files (x86)\\chromedriver-win64\\chromedriver.exe"
 # No longer need to add PATH to webdriver.Chrome(): webdriver.Chrome(PATH_TO_WEBDRIVER) is depracated.
 wd = webdriver.Chrome()
+
+def download_image(download_path, url, file_name):
+	try:
+		image_content = requests.get(url).content
+		image_file = io.BytesIO(image_content)
+		image = Image.open(image_file)
+		file_path = download_path + file_name
+
+		with open(file_path, "wb") as f:
+			image.save(f, "JPEG")
+
+		print("Success")
+	except Exception as e:
+		print('FAILED -', e)
+	
+test_img_url = r'https://www.motortrend.com/uploads/2023/08/2024-porsche-911-st-60th-anniversary-18.jpg?fit=around%7C875:492'
+download_image('./', test_img_url, file_name='Porsche.jpg')
